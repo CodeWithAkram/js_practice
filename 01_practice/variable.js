20f2c0ff44d5cab4444a0a2498c20a9128b87750
Akram Khan <akram.khan22@gmail.com>
2024-08-25 07:33:33 +0000

    commit for variables

---
+++ 01_practice/variable.js
@@ -0,0 +1,13 @@
+const accountId = 123456
+let accountEmail = 'akram@akram.com'
+var accountPassword = '123'
+accountCity = 'Noida'
+
+// accountId = '54321' can't do like this for const
+
+accountEmail = 'khan@akram.com'
+var accountPassword = '321'
+accountCity = 'Delhi'
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
